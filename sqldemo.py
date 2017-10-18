dc488e75a75dbd4c357149a0e7b2cfef9724a127
JCharante <git@jcharante.com>
2017-10-18 10:27:51 -0400

    refactor: pep 8

--- sqldemo.py
+++ sqldemo.py
@@ -5,32 +5,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
 
-
 engine = create_engine('sqlite:///:memory:', echo=True)
 
+
 def main():
-  conn = engine.connect()
-  metadata = MetaData()
-  createTables(metadata, conn)
-  s = text("SELECT users.fullname AS title FROM users")
-  statement = text("INSERT INTO users (name, fullname) values ('hal', 'Halsted Matthew Larsson')")
-  conn.execute(statement)
+    conn = engine.connect()
+    metadata = MetaData()
+    createTables(metadata, conn)
+    s = text("SELECT users.fullname AS title FROM users")
+    statement = text("INSERT INTO users (name, fullname) values ('hal', 'Halsted Matthew Larsson')")
+    conn.execute(statement)
 
-  query = text("Select * from users")
-  result = conn.execute(query).fetchall()
-  print(result)
-
-
-
-
+    query = text("Select * from users")
+    result = conn.execute(query).fetchall()
+    print(result)
 
 
 def createTables(metadata, conn):
-  users = Table('users', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('name', String),
-    Column('fullname', String))
+    users = Table('users', metadata,
+                  Column('id', Integer, primary_key=True),
+                  Column('name', String),
+                  Column('fullname', String))
 
-  metadata.create_all(engine)
+    metadata.create_all(engine)
 
 main()
