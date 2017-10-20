2207a21b40b863d62083651350ba77b2cd7f4ce8
John <git@jcharante.com>
2017-10-20 02:33:37 -0400

    indent with space

--- sqldemo.py
+++ sqldemo.py
@@ -7,49 +7,49 @@ engine = create_engine('sqlite:///:memory:', echo=True)
 
 
 def main():
-	conn = engine.connect()
-	metadata = MetaData()
-	create_tables(metadata, conn)
-	statement = text("INSERT INTO users (id, name, fullname, age)"
-									 " values (1, 'hal','Halsted Matthew Larsson', 37)")
-	conn.execute(statement)
+  conn = engine.connect()
+  metadata = MetaData()
+  create_tables(metadata, conn)
+  statement = text("INSERT INTO users (id, name, fullname, age)"
+                   " values (1, 'hal','Halsted Matthew Larsson', 37)")
+  conn.execute(statement)
 
-	statement = text("INSERT INTO users (id, name, fullname, age)"
-									 " values (2, 'Emma','Emma Doovid Judith Harringtowne', " + str(random.randint(10, 16)) + ")")
-	conn.execute(statement)
+  statement = text("INSERT INTO users (id, name, fullname, age)"
+                   " values (2, 'Emma','Emma Doovid Judith Harringtowne', " + str(random.randint(10, 16)) + ")")
+  conn.execute(statement)
 
-	statement = text("INSERT INTO pets (name, age, userid)"
-									 " values ('Josie', 4, 1)")
-	conn.execute(statement)
+  statement = text("INSERT INTO pets (name, age, userid)"
+                   " values ('Josie', 4, 1)")
+  conn.execute(statement)
 
-	statement = text("INSERT INTO pets (name, age, userid)"
-									 " values ('Bobbi', 24, 2)")
-	conn.execute(statement)
+  statement = text("INSERT INTO pets (name, age, userid)"
+                   " values ('Bobbi', 24, 2)")
+  conn.execute(statement)
 
-	statement = text("INSERT INTO pets (name, age, userid)"
-									 " values ('Bubbles', 14, 2)")
-	conn.execute(statement)
+  statement = text("INSERT INTO pets (name, age, userid)"
+                   " values ('Bubbles', 14, 2)")
+  conn.execute(statement)
 
-	query = text("SELECT users.name, pets.name from pets JOIN users ON pets.userid = users.id")
-	result = conn.execute(query).fetchall()
-	print(result)
+  query = text("SELECT users.name, pets.name from pets JOIN users ON pets.userid = users.id")
+  result = conn.execute(query).fetchall()
+  print(result)
 
 
 def create_tables(metadata, conn):
-	users = Table('users',
-								metadata,
-								Column('id', Integer, primary_key=True),
-								Column('name', String),
-								Column('age', Integer),
-								Column('fullname', String),)
+  users = Table('users',
+                metadata,
+                Column('id', Integer, primary_key=True),
+                Column('name', String),
+                Column('age', Integer),
+                Column('fullname', String),)
 
-	pets = Table('pets',
-							 metadata,
-							 Column('id', Integer, primary_key=True),
-							 Column('userId', Integer),
-							 Column('name', String),
-							 Column('age', Integer),)
-	metadata.create_all(engine)
+  pets = Table('pets',
+               metadata,
+               Column('id', Integer, primary_key=True),
+               Column('userId', Integer),
+               Column('name', String),
+               Column('age', Integer),)
+  metadata.create_all(engine)
 
 
 main()
